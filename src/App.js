ddb09a3d984b1501adae46209fe835b46c7ba418
Manas Acharekar <manas.acharekar98@gmail.com>
2020-08-28 14:01:10 +0530

    Added parsing and functionality

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import {
 	Tooltip,
 	ResponsiveContainer,
 } from 'recharts';
+import axios from 'axios';
+import moment from 'moment';
+import _ from 'underscore';
 
 import { useLoadScript } from '@react-google-maps/api';
 
@@ -25,36 +28,49 @@ import {
 
 const libraries = ['places'];
 
-const ForecastCards = () => {
-	const items = Array(5).fill({
-		day: 'Sat',
-		max: '29°',
-		min: '21°',
-		weather: 'Sunny',
-	});
+const ForecastCards = ({ data }) => {
+	const parsed = Object.entries(data)
+		.map((item) => {
+			return {
+				day: item[0],
+				main: item[1][0].main,
+				weather: item[1][0].weather[0],
+			};
+		})
+		.slice(0, 5);
+
+	if (_.isEmpty(data))
+		return (
+			<div className='flex justify-center items-center py-3 text-lg font-semibold'>
+				Loading...
+			</div>
+		);
+
 	return (
 		<div className='flex justify-evenly justify-center items-center py-3'>
-			{items.map(({ day, max, min, weather }, index) => (
+			{parsed.map(({ day, main, weather }) => (
 				<div
-					key={index}
-					className='border px-1 sm:px-3 md:px-6 md:py-3 lg:px-10 lg:py-6'>
-					<div className='flex-auto text-center px-2 md:p-0'>
-						<div className='font-semibold mt-2 text-sm md:text-lg'>
+					key={day}
+					className='sm:px-3 md:px-6 md:py-3 lg:px-10 lg:py-6'>
+					<div className='flex-auto text-center px-1 md:p-0'>
+						<div className='font-semibold mt-2 text-xs md:text-sm'>
 							{day}
 						</div>
-						<div className='text-sm md:text-lg'>
-							<span className='font-semibold mr-2'>{max}</span>
-							{min}
+						<div className='text-xs md:text-sm'>
+							<span className='font-semibold mr-2'>
+								{Math.round(main.temp_max)}°C
+							</span>
+							{Math.round(main.temp_min)}°C
 						</div>
 						<div className='flex justify-center'>
 							<img
-								src='images/sun.svg'
+								src={`images/${weather.icon}.svg`}
 								alt='weather condition'
-								className='w-8 md:w-12 lg:w-16 mt-2'
+								className='w-4 md:w-12 lg:w-16 mt-2'
 							/>
 						</div>
-						<div className='my-2 text-gray-600 text-sm md:text-lg'>
-							{weather}
+						<div className='my-2 text-gray-600 text-xs md:text-sm'>
+							{weather.main}
 						</div>
 					</div>
 				</div>
@@ -63,67 +79,101 @@ const ForecastCards = () => {
 	);
 };
 
-const DetailCard = () => {
-	const data = [
-		{
-			name: 'Page A',
-			uv: 4000,
-			pv: 2400,
-			amt: 2400,
-		},
-		{
-			name: 'Page B',
-			uv: 3000,
-			pv: 1398,
-			amt: 2210,
-		},
-		{
-			name: 'Page C',
-			uv: 2000,
-			pv: 9800,
-			amt: 2290,
-		},
-		{
-			name: 'Page D',
-			uv: 2780,
-			pv: 3908,
-			amt: 2000,
-		},
-		{
-			name: 'Page E',
-			uv: 1890,
-			pv: 4800,
-			amt: 2181,
-		},
-		{
-			name: 'Page F',
-			uv: 2390,
-			pv: 3800,
-			amt: 2500,
-		},
-		{
-			name: 'Page G',
-			uv: 3490,
-			pv: 4300,
-			amt: 2100,
-		},
-	];
+const DetailCard = ({ data, details }) => {
+	const { sunrise, sunset } = details;
+	// const {} = data[0];
+	const parsed = Object.entries(data)
+		.map((item) => {
+			return {
+				day: item[0],
+				main: item[1][0].main,
+				weather: item[1][0].weather[0],
+			};
+		})
+		.slice(0, 5);
+
+	const graph = Object.entries(data).map((item) => {
+		return {
+			day: item[0],
+			data: item[1].map((obj) => {
+				return {
+					temp: obj.main.temp,
+					time: moment.unix(obj.dt).format('LT'),
+				};
+			}),
+		};
+	});
+	console.log(graph);
+	// const graph = [
+	// 	{
+	// 		name: 'Page A',
+	// 		uv: 4000,
+	// 		pv: 2400,
+	// 		amt: 2400,
+	// 	},
+	// 	{
+	// 		name: 'Page B',
+	// 		uv: 3000,
+	// 		pv: 1398,
+	// 		amt: 2210,
+	// 	},
+	// 	{
+	// 		name: 'Page C',
+	// 		uv: 2000,
+	// 		pv: 9800,
+	// 		amt: 2290,
+	// 	},
+	// 	{
+	// 		name: 'Page D',
+	// 		uv: 2780,
+	// 		pv: 3908,
+	// 		amt: 2000,
+	// 	},
+	// 	{
+	// 		name: 'Page E',
+	// 		uv: 1890,
+	// 		pv: 4800,
+	// 		amt: 2181,
+	// 	},
+	// 	{
+	// 		name: 'Page F',
+	// 		uv: 2390,
+	// 		pv: 3800,
+	// 		amt: 2500,
+	// 	},
+	// 	{
+	// 		name: 'Page G',
+	// 		uv: 3490,
+	// 		pv: 4300,
+	// 		amt: 2100,
+	// 	},
+	// ];
+
+	if (_.isEmpty(data))
+		return (
+			<div className='flex justify-center items-center py-3 text-lg font-semibold'>
+				Loading...
+			</div>
+		);
+
 	return (
 		<div className='border shadow-xl rounded p-4'>
 			<div className='flex items-center content-center justify-start'>
-				<div className='text-6xl font-extrabold mr-4'>26°C</div>
+				<div className='text-6xl font-extrabold mr-4'>
+					{Math.round(parsed[0].main.temp)}°C
+				</div>
 				<div>
 					<img
-						src='images/sun.svg'
+						src={`images/${parsed[0].weather.icon}.svg`}
 						alt='weather condition'
 						className='w-16'
 					/>
 				</div>
 			</div>
 
-			<div className='p-8'>
+			<div className='p-2'>
 				<ResponsiveContainer width='100%' height={250}>
-					<AreaChart data={data}>
+					<AreaChart data={graph[1].data}>
 						<defs>
 							<linearGradient
 								id='colorUv'
@@ -143,13 +193,13 @@ const DetailCard = () => {
 								/>
 							</linearGradient>
 						</defs>
-						<XAxis />
+						<XAxis dataKey='time' />
 						<YAxis hide />
 						<CartesianGrid horizontal={false} />
 						<Tooltip />
 						<Area
 							type='monotone'
-							dataKey='uv'
+							dataKey='temp'
 							stroke='#7bbae8'
 							fillOpacity={1}
 							fill='url(#colorUv)'
@@ -160,38 +210,46 @@ const DetailCard = () => {
 
 			<div className='flex justify-center justify-center p-2 md:p-4'>
 				<div className='w-1/2 bg-gray-200 rounded mr-6 px-4 py-4 md:px-4 md:py-6 text-left'>
-					<div className='font-extrabold text-lg md:text-xl'>
+					<div className='font-extrabold text-base md:text-xl'>
 						Pressure
 					</div>
-					<div className='font-light text-lg md:text-xl mt-2'>
-						1013 hpa
+					<div className='font-light text-base md:text-xl mt-2'>
+						{parsed[0].main.pressure} hpa
 					</div>
 				</div>
 				<div className='w-1/2 bg-gray-200 rounded ml-6 px-4 py-4 md:px-4 md:py-6 text-left'>
-					<div className='font-extrabold text-lg md:text-xl'>
+					<div className='font-extrabold text-base md:text-xl'>
 						Humidity
 					</div>
-					<div className='font-light text-lg md:text-xl mt-2'>
-						93 %
+					<div className='font-light text-base md:text-xl mt-2'>
+						{parsed[0].main.humidity} %
 					</div>
 				</div>
 			</div>
 
 			<div className='flex justify-between justify-center p-4'>
 				<div className='text-left'>
-					<div className='font-extrabold md:text-lg'>Sunrise</div>
-					<div className='md:text-lg'>7:22am</div>
+					<div className='font-extrabold text-sm md:text-lg'>
+						Sunrise
+					</div>
+					<div className='font-light text-sm md:text-lg'>
+						{moment.unix(sunrise).format('LT')}
+					</div>
 				</div>
 				<div className='text-right'>
-					<div className='font-extrabold md:text-lg'>Sunset</div>
-					<div className='md:text-lg'>6:12pm</div>
+					<div className='font-extrabold text-sm md:text-lg'>
+						Sunset
+					</div>
+					<div className='font-light text-sm md:text-lg'>
+						{moment.unix(sunset).format('LT')}
+					</div>
 				</div>
 			</div>
 		</div>
 	);
 };
 
-const SearchBar = () => {
+const SearchBar = ({ setPlace }) => {
 	const {
 		ready,
 		value,
@@ -217,8 +275,8 @@ const SearchBar = () => {
 
 					try {
 						const result = await getGeocode({ address });
-						console.log(result);
 						const { lat, lng } = await getLatLng(result[0]);
+						setPlace({ lat, lng });
 					} catch (error) {
 						console.log(error);
 					}
@@ -242,7 +300,6 @@ const SearchBar = () => {
 			</Combobox>
 		</div>
 	);
-	// return <div></div>;
 };
 
 const App = () => {
@@ -251,19 +308,55 @@ const App = () => {
 		libraries,
 	});
 
-	if (loadError) return <div>Error loading maps</div>;
+	const [place, setPlace] = React.useState({ lat: 19.0144, lng: 72.8479 });
+	const [weatherData, setWeatherData] = React.useState({});
+	const [extraDetails, setExtraDetails] = React.useState({});
+
+	React.useEffect(() => {
+		async function fetchData() {
+			axios
+				.post(
+					`http://api.openweathermap.org/data/2.5/forecast?lat=${place.lat}&lon=${place.lng}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+				)
+				.then((res) => {
+					const groups = _.groupBy(res.data.list, function (item) {
+						return moment(item.dt_txt).format('dddd');
+					});
+					setExtraDetails(res.data.city);
+					setWeatherData(groups);
+				});
+		}
+
+		fetchData();
+	}, [place]);
+
+	React.useEffect(() => {
+		navigator.geolocation.getCurrentPosition(
+			(position) => {
+				setPlace({
+					lat: position.coords.latitude,
+					lng: position.coords.longitude,
+				});
+			},
+			(error) => {
+				console.log(error);
+			}
+		);
+	}, []);
+
+	if (loadError) return <div>Error loading application!</div>;
 	if (!isLoaded) return <div>Loading Maps</div>;
 
 	return (
 		<div className='container mx-auto md:px-12 lg:px-16 xl:px-20'>
 			<div className='p-4 flex justify-center'>
-				<SearchBar />
+				<SearchBar setPlace={setPlace} />
 			</div>
 			<div>
-				<ForecastCards />
+				<ForecastCards data={weatherData} />
 			</div>
 			<div className='p-4'>
-				<DetailCard />
+				<DetailCard data={weatherData} details={extraDetails} />
 			</div>
 		</div>
 	);
